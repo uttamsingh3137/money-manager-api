5aae4638fdb83d2dd4ece8802691fdaf35b8967f
uttamsingh3137 <uttampratp9871@gmail.com>
2025-08-10 03:26:50 +0530

    initial commit

---
+++ src/main/java/in/bushansirgur/moneymanager/controller/CategoryController.java
@@ -0,0 +1,44 @@
+package in.bushansirgur.moneymanager.controller;
+
+import in.bushansirgur.moneymanager.dto.CategoryDTO;
+import in.bushansirgur.moneymanager.entity.CategoryEntity;
+import in.bushansirgur.moneymanager.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<CategoryDTO> saveCategory(@RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO savedCategory = categoryService.saveCategory(categoryDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedCategory);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryDTO>> getCategories() {
+        List<CategoryDTO> categories = categoryService.getCategoriesForCurrentUser();
+        return ResponseEntity.ok(categories);
+    }
+
+    @GetMapping("/{type}")
+    public ResponseEntity<List<CategoryDTO>> getCategoriesByTypeForCurrentUser(@PathVariable String type) {
+        List<CategoryDTO> list = categoryService.getCategoriesByTypeForCurrentUser(type);
+        return ResponseEntity.ok(list);
+    }
+
+    @PutMapping("/{categoryId}")
+    public ResponseEntity<CategoryDTO> updateCategory(@PathVariable Long categoryId,
+                                                    @RequestBody CategoryDTO categoryDTO) {
+        CategoryDTO updatedCategory = categoryService.updateCategory(categoryId, categoryDTO);
+        return ResponseEntity.ok(updatedCategory);
+    }
+}
---
+++ src/main/java/in/bushansirgur/moneymanager/controller/IncomeController.java
@@ -0,0 +1,37 @@
+package in.bushansirgur.moneymanager.controller;
+
+import in.bushansirgur.moneymanager.dto.ExpenseDTO;
+import in.bushansirgur.moneymanager.dto.IncomeDTO;
+import in.bushansirgur.moneymanager.service.IncomeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/incomes")
+public class IncomeController {
+
+    private final IncomeService incomeService;
+
+    @PostMapping
+    public ResponseEntity<IncomeDTO> addExpense(@RequestBody IncomeDTO dto) {
+        IncomeDTO saved = incomeService.addIncome(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<IncomeDTO>> getExpenses() {
+        List<IncomeDTO> expenses = incomeService.getCurrentMonthIncomesForCurrentUser();
+        return ResponseEntity.ok(expenses);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteIncome(@PathVariable Long id) {
+        incomeService.deleteIncome(id);
+        return ResponseEntity.noContent().build();
+    }
+}
